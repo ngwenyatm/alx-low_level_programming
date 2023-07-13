ef90f794f3244cf8750f24d18b4d8d895ca7f9e6
MSI293 <thabsnspaces@gmail.com>
2023-07-13 13:52:06 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * random comments here
+ *main - random comments here
  *
- * just for practice
+ *Return:0 (Success)
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
