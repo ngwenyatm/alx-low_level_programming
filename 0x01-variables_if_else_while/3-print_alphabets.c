da6fe8db7690cccfc8fa382d8244f7ba7298af7b
MSI293 <thabsnspaces@gmail.com>
2023-07-14 15:51:08 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,11 @@ int main(void)
 {
 	char Small_char = 'a';
 	char Big_char = 'A';
-	
 	while (Small_char <= 'z')
 	{
 	putchar(Small_char);
 	Small_char++;
 	}
-	
 	while (Big_char <= 'Z')
 	{
 		putchar(Big_char);
