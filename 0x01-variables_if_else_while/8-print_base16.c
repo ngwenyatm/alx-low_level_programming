a359b356999f1e8962a0cb0b65535bc97ed9c54a
MSI293 <thabsnspaces@gmail.com>
2023-07-15 12:17:27 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - base 16
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
