645f48730e7fcb34d2e722d44524187142b6c310
MSI293 <thabsnspaces@gmail.com>
2023-07-15 11:46:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 0; j++)
+	{
+		putchar(j + 48);
+	}
+	putchar('\n');
+	return (0);
+}
