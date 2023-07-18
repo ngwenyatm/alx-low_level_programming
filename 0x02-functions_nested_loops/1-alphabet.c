61da6f6623ee63c95e32c1de1174cd1cf3cefad6
MSI293 <thabsnspaces@gmail.com>
2023-07-18 11:05:20 -0700

    pushed with script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
+ * print_alphabet - Entry point
+ * Return:0
  */
 
-	void print_alphabet(void)
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		char letter;
+		_putchar(c);
+		c++;
 
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
 	}
+	_putchar('\n');
+
+}
 
