b49016aa9ca7adc1a5e0bedf0ab9dcd79ee1a765
MSI293 <thabsnspaces@gmail.com>
2023-07-18 13:12:27 -0700

    pushed with script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include "main.h"
+/**
+ * add - adds 2 numbers and returns
+ *
+ * @i: number being added
+ *
+ * @j: number being added
+ *
+ * Return: Always 0.
+ *
+ */
+
+
+int add(int i, int j)
+{
+	return (i + j);
+}
+
