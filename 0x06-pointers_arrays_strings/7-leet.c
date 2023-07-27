685e5ab78ecafea0aebe389294dd0fa3fb60500c
MSI293 <thabsnspaces@gmail.com>
2023-07-27 11:00:17 -0700

    pushed with script

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet -  function that encodes a string into 1337
+ * @str: string
+ * Return: new str value
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
+
