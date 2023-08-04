b0c772f8079c3214f4729cfeaa7231244c3e2b07
MSI293 <thabsnspaces@gmail.com>
2023-08-04 13:21:03 -0700

    pushed with script

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,14 +5,16 @@
  * main - prints all arguments it receives
  * @argc: number of args
  * @argv: array
- *
  * Return: Always 0
- */
-
+ * */
 int main(int argc, char *argv[])
 {
-	(void) argv;
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
