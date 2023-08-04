640d07ce1fd4999bb3d723e81f935dd2ed7d3bf0
MSI293 <thabsnspaces@gmail.com>
2023-08-04 12:46:08 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,9 @@
  * @argv: array of arg
  * Return: Always 0 
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
