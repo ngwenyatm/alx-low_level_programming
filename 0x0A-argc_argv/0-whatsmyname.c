670eda25cc90cfc4016e2d75f2fd55c4f083e7d0
ngwenyatm <thabsnspaces@gmail.com>
2023-09-29 14:36:41 -0700

    pushed with script

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,10 @@
  * main - prints the name of the program then new line
  * @argc: number of arg
  * @argv: array of arg
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
 	return (0);
 }
