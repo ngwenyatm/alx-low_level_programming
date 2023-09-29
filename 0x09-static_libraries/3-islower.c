e70416fdc5ba2ace2a948d2155093dde6a3dcf70
ngwenyatm <thabsnspaces@gmail.com>
2023-09-29 02:58:34 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a char is lowercase
+ * @c: the char to check
+ * Return: 1 if lowercase 0 if not
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Finds the first occurrence of a string
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *str, char *sub)
+{
+	for (; *str != '\0'; str++)
+	{
+		char *i = str;
+		char *j = sub;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (str);
+	}
+
+	return (0);
+}
+
