7fbfbe90a059383ac31821fac657fd816858d8c7
ngwenyatm <thabsnspaces@gmail.com>
2023-10-09 08:02:01 -0700

    preprocessor work

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the current working file
+ *
+ * Return: Always 0 (On Success)
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
