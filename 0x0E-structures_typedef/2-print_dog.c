feab0d2a642d58f116c49d694588011db6f188ce
ngwenyatm <thabsnspaces@gmail.com>
2023-10-10 07:22:30 -0700

    pushed with script

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,14 +8,14 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
 	if (d == NULL)
 		return;
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
