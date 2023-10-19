463824e09ae264bf9d26267bba0ef16444cfff0c
ngwenyatm <thabsnspaces@gmail.com>
2023-10-19 13:51:58 -0700

    pushed with script

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,39 +6,43 @@
  * @str: string to be stored in the list_t list.
  * Return: the address of newly added node
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode;
 
-	list_t *holder;
+	list_t *hold;
+
+	hold = *head;
+
+	int count = 0;
+
+	while (str[count])
+		count++;
 
 	newNode = malloc(sizeof(list_t));
 
 	if (newNode == NULL)
 		return (NULL);
-	
-	if (str == NULL)
-		return (NULL);
 
 	newNode->str = strdup(str);
 
-	for (size_t count = 0; str[count]; count++)
-
 	newNode->len = count;
-	newNode->next = NULL;
-	holder = *head;
 
-	if (holder == NULL)
+	newNode->next = NULL;
+
+	if (*head == NULL)
 	{
 		*head = newNode;
 		return (newNode);
 	}
-	else
-	{
-		while(holder->next != NULL)
-			holder = holder->next;
-			holder->next = newNode;
-	}
+
+	while (hold->next != NULL)
+
+	hold = hold->next;
+
+	hold->next = newNode;
 
 	return (newNode);
 }
+
