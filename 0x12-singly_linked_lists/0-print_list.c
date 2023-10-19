c43dd705886c0cc4a256f9751a3f2ac7212a20e2
ngwenyatm <thabsnspaces@gmail.com>
2023-10-19 09:25:34 -0700

    pushed with script

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "lists.h"
 
 /**
  * print_list - prints all the elements of a linked list
@@ -14,7 +14,7 @@ u{
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0]" "(nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
