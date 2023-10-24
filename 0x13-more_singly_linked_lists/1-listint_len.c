2b4678820649e157bdc08d5469b3f8a8e661e639
ngwenyatm <thabsnspaces@gmail.com>
2023-10-24 04:55:12 -0700

    pushed with script

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - returns the number of elements in a linked listint_t list.
+ * @h: pointer to listint_t list to print
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
