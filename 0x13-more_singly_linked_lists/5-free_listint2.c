e4b176eabe7db75ba08bfce87f6c4811e8735569
ngwenyatm <thabsnspaces@gmail.com>
2023-10-24 09:40:21 -0700

    pushed with script

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+/**
+ * free_listint -  frees a listint_t list.
+ * @head: listint_t list to free
+ */
+void free_listint(listint_t *head)
+{
+ listint_t *hold;
+
+ while (head)
+ {
+ hold = head->next;
+
+ free(head);
+
+ head = hold;
+
+ }
+ *head = NULL;
+}
