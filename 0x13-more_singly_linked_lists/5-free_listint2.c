4a6cfaff4bf32af42eec6f314dec7c571dc8b5be
ngwenyatm <thabsnspaces@gmail.com>
2023-10-24 09:53:13 -0700

    pushed with script

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - frees a listint_t list
+ * @head: listint_t list to free
  */
 void free_listint2(listint_t **head)
 {
