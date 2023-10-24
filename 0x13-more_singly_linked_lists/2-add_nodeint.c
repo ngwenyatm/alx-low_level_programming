9c5b54acd494ce9169c9acd29544b9135cb43ab2
ngwenyatm <thabsnspaces@gmail.com>
2023-10-24 05:00:49 -0700

    pushed with script

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the address of list_t list
+ * @str: string to add to the new node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	int count = 0;
+
+	while (str[count])
+		count++;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = count;
+	newNode->next = (*head);
+	(*head) = newNode;
+
+	return (*head);
+}
