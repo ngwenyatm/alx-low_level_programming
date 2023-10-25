c5f1fc17632f497bb00102efc39f557a99a7a558
ngwenyatm <thabsnspaces@gmail.com>
2023-10-25 06:39:52 -0700

    pushed with script

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+
+#include "lists.h"
+
+/**
+ * free_listint_safe -  frees a listint_t list
+ * @h: pointer to the first node in a listint_t list
+ *
+ * Return: number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int count = 0;
+	listint_t *hold;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	while (*h != NULL)
+	{
+		if ((*h - (*h)->next) > 0)
+		{
+			hold = ((*h)->next);
+			free(*h);
+			*h = hold;
+
+			count++;
+		}
+
+		else
+		{
+			free(*h);
+			*h = NULL;
+
+			count++;
+
+			break;
+		}
+	}
+
+	*h = NULL;
+	return (count);
+}
