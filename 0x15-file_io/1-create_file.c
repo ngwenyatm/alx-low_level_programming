81bb24a7eba880c6fb14d2de38792885bb2a03ed
ngwenyatm <thabsnspaces@gmail.com>
2023-10-30 11:03:28 -0700

    pushed with script

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file.
+ * @text_content: content to be written into the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fileD, writ;
+int count = 0;
+
+if (filename == NULL)
+return (-1);
+
+fileD = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+writ = (fileD, text_content, len);
+
+if (fileD == -1 || writ == -1)
+return (-1);
+
+close(fileD);
+
+return (1);
+
+}
