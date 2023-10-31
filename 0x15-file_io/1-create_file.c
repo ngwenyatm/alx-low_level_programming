020f714b57c17b4431e1ec2a807d40517c957bfe
ngwenyatm <thabsnspaces@gmail.com>
2023-10-31 01:39:56 -0700

    pushed with script

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int fileD, writ;
-int count = 0;
+int count;
 
 if (filename == NULL)
 return (-1);
@@ -20,8 +20,8 @@ fileD = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fileD == -1)
 	return (-1);
 
-while (text_content[count])
-	count++;
+for (;text_content[count];count++)
+	;
 
 writ = write(fileD, text_content, count);
 
