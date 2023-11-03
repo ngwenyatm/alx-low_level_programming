962bee6712e9ad14b8cb424228d1d9c41225036a
ngwenyatm <thabsnspaces@gmail.com>
2023-11-03 03:46:56 -0700

    pushed with script

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 			exit(98);
 	}
 
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
