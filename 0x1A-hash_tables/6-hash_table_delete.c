05665489652e84d9570f8c2740788de0ff9d5c25
ngwenyatm <thabsnspaces@gmail.com>
2024-02-16 12:46:43 -0800

    pushed with script

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
+#include "hash_tables.h"
 
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: pointer to a hash table.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
     if (ht == NULL) {
