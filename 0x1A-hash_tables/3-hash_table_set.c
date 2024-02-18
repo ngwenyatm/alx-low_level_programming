a35c7e34df27040da16ede7343e9f90dec8bd244
ngwenyatm <thabsnspaces@gmail.com>
2024-02-18 04:43:39 -0800

    pushed with script

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,14 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
+
 	val = strdup(value);
-	
+
 	if (val == NULL)
 		return (0);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
-	
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -31,7 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	
 	tmp = malloc(sizeof(hash_node_t));
 	if (tmp == NULL)
 	{
