1c2b65b1503d37f8ba8018096a8502b08da1ec96
ngwenyatm <thabsnspaces@gmail.com>
2024-02-18 05:38:07 -0800

    pushed with script

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,54 +7,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
-		return 0;
-	}
-	
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *tmp = NULL, *prev = NULL, *current = ht->array[index];
-	
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-		{
-			free(current->value);
-			current->value = strdup(value);
-			return 1;
-		}
-		prev = current;
-		current = current->next;
-	}
+       hash_node_t *tmp;
+       char *val;
+       unsigned long int index, i;
 
-	tmp = malloc(sizeof(hash_node_t));
-	if (tmp == NULL)
-	{
-		return (0);
-	}
-	
-	tmp->key = strdup(key);
-	if (tmp->key == NULL)
-	{
-		free(tmp);
-		return 0;
-	}
-	tmp->value = strdup(value);
-	
-	if (tmp->value == NULL)
-	{
-		free(tmp->key);
-		free(tmp);
-		return 0;
-	}
-	
-	if (prev != NULL)
-	{
-		prev->next = tmp;
-	}
-	else
-	{
-		ht->array[index] = tmp;
-	}
-	return (1);
+       if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+               return (0);
+
+       val = strdup(value);
+
+       if (val == NULL)
+               return (0);
+
+       index = key_index((const unsigned char *)key, ht->size);
+       for (i = index; ht->array[i]; i++)
+       {
+               if (strcmp(ht->array[i]->key, key) == 0)
+               {
+                       free(ht->array[i]->value);
+                       ht->array[i]->value = val;
+
+                       return (1);
+               }
+       }
+       tmp = malloc(sizeof(hash_node_t));
+       if (tmp == NULL)
+       {
+               free(val);
+               return (0);
+       }
+
+       tmp->key = strdup(key);
+       if (tmp->key == NULL)
+       {
+               free(tmp);
+               return (0);
+       }
+       tmp->value = val;
+       tmp->next = ht->array[index];
+       ht->array[index] = tmp;
+       return (1);
 }
