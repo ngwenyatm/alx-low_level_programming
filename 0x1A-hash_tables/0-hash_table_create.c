d10ba896104c421b3b58db97d69d75ee417b4333
ngwenyatm <thabsnspaces@gmail.com>
2024-02-18 04:11:09 -0800

    pushed with script

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,26 +6,30 @@
  * Return: Pointer to the newly created hash table, or NULL on failure.
  */
 
-hash_table_t *hash_table_create(unsigned long int size) {
-    hash_table_t *h_table;
-    unsigned long int i;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table;
+	unsigned long int i;
 
-    h_table = malloc(sizeof(hash_table_t));
-    if (!h_table) {
-        return NULL;
-    }
+	h_table = malloc(sizeof(hash_table_t));
 
-    h_table->size = size;
+	if (!h_table)
+	{
+		return (NULL);
+	}
 
-    h_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (!h_table->array) {
-        free(h_table);
-        return NULL;
-    }
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
 
-    for (i = 0; i < size; i++) {
-        h_table->array[i] = NULL;
-    }
+	if (!h_table->array)
+	{
+		free(h_table);
+		return (NULL);
+	}
 
-    return h_table;
+	for (i = 0; i < size; i++)
+	{
+		h_table->array[i] = NULL;
+	}
+	return (h_table);
 }
