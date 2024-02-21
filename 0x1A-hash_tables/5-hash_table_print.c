a027542dab41a40f42fb149b9304b1cbcefd9702
ngwenyatm <thabsnspaces@gmail.com>
2024-02-21 13:10:27 -0800

    pushed with script

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,26 +4,31 @@
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to hash table
 */
+void hash_table_print(const hash_table_t *ht) {
+	unsigned long int i;
+	bool needs_comma = true;
+	if (ht == NULL) {
+    return;
+  }
 
-void hash_table_print(const hash_table_t *ht)
-{
-    if (ht == NULL) {
-        return;
-		    }
+  printf("{\n");
 
-    printf("{");
-
-   
-    for (size_t i = 0; i < ht->size; i++) {
-        hash_node_t *node = ht->array[i];
-        while (node) {
-            printf("'%s': '%s'", node->key, node->value);
-            node = node->next;
-            if (node) {  
-                printf(", ");
-            }
+ for (i = 0; i < ht->size; i++) {
+    if (ht->array[i] != NULL) {
+      needs_comma = false;
+      node = ht->array[i];
+      while (node != NULL) {
+        printf("'%s': '%s'", node->key, node->value);
+        if (node->next != NULL) {
+          needs_comma = true;
         }
+        if (needs_comma) {
+          printf(", ");
+          needs_comma = false;
+        }
+        node = node->next;
+      }
     }
-
-    printf("}\n");
+  }
+  printf("}\n");
 }
