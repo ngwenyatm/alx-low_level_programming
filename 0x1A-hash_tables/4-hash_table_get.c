0936d111f8a208f44e3b991da0b69dbda5acf1e6
ngwenyatm <thabsnspaces@gmail.com>
2024-02-21 12:47:35 -0800

    pushed with script

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,22 +8,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *tmp;
-	unsigned long int i;
-
-	if (key == NULL || ht == NULL || *key == '\0')
+	hash_node_t  *hold;
+	unsigned long int i = 0;
+	if (!ht || key == NULL || !*key)
 		return (NULL);
-
 	i = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
-	tmp = ht->array[i];
-	while (tmp && strcmp(tmp->key, key) != 0)
-		tmp = tmp->next;
-
-	if (tmp == NULL)
-		return NULL
-	else 
-		return tmp->value;
+	hold = ht->array[i];
+	while (hold)
+	{
+		if (strcmp(key, hold->key) != 0)
+			return (hold->value);
+		hold = hold->next;
+	}
+	return (NULL);
 }
