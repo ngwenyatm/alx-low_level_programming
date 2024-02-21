cba56ef9315130f1a74b3d7c7767afdcc8c0ed87
ngwenyatm <thabsnspaces@gmail.com>
2024-02-21 12:39:36 -0800

    pushed with script

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,33 +7,47 @@
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
-    if (ht != NULL || !key || !value) {
-        return 0;
+  unsigned long int i = 0;
+  char *value_cp, *key_cp;
+  hash_node_t *hold, *node;
+
+  if (!key || !ht) {
+    return 0;
+  }
+  if (!*key || !value) {
+    return 0;
+  }
+  value_cp = malloc(strlen(value) + 1);
+  if (value_cp == NULL) {
+    return 0;
+  }
+  key_cp = malloc(strlen(key) + 1);
+  if (key_cp == NULL) {
+    free(value_cp);
+    return 0;
+  }
+  strcpy(value_cp, value);
+  strcpy(key_cp, key);
+  i = key_index((const unsigned char *)key, ht->size);
+  hold = ht->array[i];
+  while (hold) {
+    if (!strcmp(key, hold->key)) {
+      free(hold->value);
+      hold->value = value_cp;
+      free(key_cp);
+      return 1;
     }
-
-    unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
-
-    hash_node_t *tmp = ht->array[index];
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0) {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            return (1);
-	}
-        tmp = tmp->next;
-    }
-
-    hash_node_t *node = malloc(sizeof(hash_node_t));
-    if (node != NULL)
-    {
-        return 0;
-    }
-
-    node->key = strdup(key);
-    node->value = strdup(value);
-    node->next = ht->array[index];
-    ht->array[index] = node;
-
-    return (1);
-}
+    hold = hold->next;
+  }
+  node = malloc(sizeof(hash_node_t));
+  if (node == NULL) {
+    free(value_cp);
+    free(key_cp);
+    return 0;
+  }
+  node->key = key_cp;
+  node->value = value_cp;
+  node->next = ht->array[i];
+  ht->array[i] = node;
+  return 1;
+}   
