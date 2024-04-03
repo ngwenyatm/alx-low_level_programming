595ea1b744ff53968290d0a0e007a70a49cb5e7a
ngwenyatm <thabsnspaces@gmail.com>
2024-04-03 09:30:54 -0700

    pushed with script

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Args:
+        grid: A list of lists of ints,
+        where 0 = water and 1 = land.
+
+    Return:
+        Perimeter of the island.
+    """
+    isl_perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                isl_perimeter = isl_perimeter + 4
+                if j > 0 and grid[i][j-1] == 1:
+                    isl_perimeter = isl_perimeter + 2
+                if i > 0 and grid[i-1][j] == 1:
+                    isl_perimeter = isl_perimeter + 2
+    return isl_perimeter
