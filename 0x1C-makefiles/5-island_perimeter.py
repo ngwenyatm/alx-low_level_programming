3249ea7d85eb68843754e6c602b569dc36a403aa
ngwenyatm <thabsnspaces@gmail.com>
2024-04-03 09:37:54 -0700

    pushed with script

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Calculates the perimeter of an island from grid.
+"""
 
 def island_perimeter(grid):
     """
@@ -9,14 +12,15 @@ def island_perimeter(grid):
     Return:
         Perimeter of the island.
     """
+
     isl_perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                isl_perimeter = isl_perimeter + 4
+                isl_perimeter = isl_perimeter+4
                 if j > 0 and grid[i][j-1] == 1:
-                    isl_perimeter = isl_perimeter + 2
+                    isl_perimeter = isl_perimeter-2
                 if i > 0 and grid[i-1][j] == 1:
-                    isl_perimeter = isl_perimeter + 2
+                    isl_perimeter = isl_perimeter+2
     return isl_perimeter
