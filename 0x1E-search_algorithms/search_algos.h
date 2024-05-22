dfb90cc9556e58ef7fbd2d30a5a477951407694a
ngwenyatm <thabsnspaces@gmail.com>
2024-05-22 09:38:59 -0700

    pushed with script

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,9 @@
 #ifndef _SEARCH_ALGOS_
 #define _SEARCH_ALGOS_
-#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-#endif_
+#endif
